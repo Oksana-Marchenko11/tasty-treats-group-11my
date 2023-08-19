03b93c546d287669d07ca2bbb3d225baf8c86389
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-08-20 01:24:56 +0300

    some css + fix-filter

--- src/js/testyApiService.js
+++ src/js/testyApiService.js
@@ -3,60 +3,70 @@ import axios from 'axios';
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
 
 export default class TestyApiService {
-  constructor() {
-    this.params = {};
-    this.params.category = '';
-    this.params.limit = 9;
-    this.params.title = '';
-    this.params.ingredient = '';
-    this.params.area = '';
-    this.params.time = '';
-  }
+    constructor() {
+        this.params = {};
+        this.params.category = '';
+        this.params.limit = '';
+        this.params.title = '';
+        this.params.ingredient = '';
+        this.params.area = '';
+        this.params.time = '';
+    }
 
-  async getCategories() {
-    const response = await axios.get(`${BASE_URL}/categories`);
-    return response.data;
-  }
+    async getCategories() {
+        const response = await axios.get(
+            `${BASE_URL}/categories`
+        );
+        return response.data;
+    }
 
-  setCategory(name) {
-    this.params.category = name;
-  }
+    setCategory(name) {
+        this.params.category = name;
+    }
 
-  setPerPage(num) {
-    this.params.limit = num;
-  }
+    setPerPage(num) {
+        this.params.limit = num;
+    }
 
-  setSearchText(text) {
-    this.params.title = text;
-  }
+    setSearchText(text) {
+        this.params.title = text;
+    }
 
-  setSearchIngredient(ingredient) {
-    this.params.ingredient = ingredient;
-  }
-  setSearchArea(area) {
-    this.params.area = area;
-  }
+    setSearchIngredient(ingredient) {
+        this.params.ingredient = ingredient;
+    }
+    setSearchArea(area) {
+        this.params.area = area;
+    }
 
-  async getRecipes() {
-    const response = await axios.get(`${BASE_URL}/recipes`, {
-      params: this.params,
-    });
-    return response.data;
-  }
+    async getRecipes() {
+        const response = await axios.get(
+            `${BASE_URL}/recipes`
+            , {
+                params: this.params
+            }
+        );
+        return response.data;
+    }
 
-  async getAreas() {
-    const response = await axios.get(`${BASE_URL}/areas`);
-    console.log(response.data);
-    return response.data;
-  }
+    async getAreas() {
+        const response = await axios.get(
+            `${BASE_URL}/areas`
+        );
+        console.log(response.data)
+        return response.data;
+    }
 
-  async getIngredients() {
-    const response = await axios.get(`${BASE_URL}/ingredients`);
-    console.log(response.data);
-    return response.data;
-  }
+    async getIngredients() {
+        const response = await axios.get(
+            `${BASE_URL}/ingredients`
+        );
+        console.log(response.data)
+        return response.data;
+    }
 
-  setSearchTime(time) {
-    this.params.time = time;
-  }
+    setSearchTime(time) {
+        this.params.time = time;
+    }
+
 }
