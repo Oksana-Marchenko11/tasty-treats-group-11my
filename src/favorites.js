d201ba3f76bb817475014bc919213e442d0b473a
lera2342 <valeriiapylypenko3@gmail.com>
2023-08-19 22:30:06 +0300

    commit-lera-favourit

---
+++ src/favorites.js
@@ -0,0 +1,16 @@
+
+import TestyApiService from './js/testyApiService';
+const testyApiService = new TestyApiService();
+
+const categoryListFavourite = document.querySelector(".category-list-favourites");
+
+categoryListFavourite.addEventListener('click', function (e) {
+  console.log(e.target.dataset.recipeCategory);
+  testyApiService.setCategory(e.target.dataset.recipeCategory);
+  renewRecipes();
+});
+testyApiService.getCategories().then(data => {
+  data.forEach(category => {
+    categoryListFavourite.innerHTML += `<li><button class="category-btn" data-recipe-category="${category.name}">${category.name}</button></li>`;
+  });
+});
