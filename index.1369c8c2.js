66c23856326162e05db1c9e1a7992a32d7fa222a
AnastasiiaPavlovska <126072548+AnastasiiaPavlovska@users.noreply.github.com>
2023-08-20 16:20:20 +0000

    Deploying to gh-pages from @ AnastasiiaPavlovska/tasty-treats-group-11@e599b830bd5b3326376087898033015fb38eb576 🚀

---
+++ index.1369c8c2.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-recipe-open]"),closeModalBtn:document.querySelector("[data-modal-recipe-close]"),modal:document.querySelector("[data-modal-recipe]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.1369c8c2.js.map
---
+++ index.2afbdf4b.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-order-open]"),closeModalBtn:document.querySelector("[data-modal-order-close]"),modal:document.querySelector("[data-modal-order]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.2afbdf4b.js.map
