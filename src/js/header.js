928c76415c5f466db05b074c59d29ca43e61faf6
turko-yurii <turkoburko1993@gmail.com>
2023-08-23 07:13:11 +0300

    header/dark/light/mode

--- src/js/header.js
+++ src/js/header.js
@@ -29,6 +29,40 @@ window.addEventListener('click', e => {
     }
 });
 
+function getTheme() {
+    return localStorage.getItem('theme') || 'light';
+  }
+  function saveTheme(theme) {
+    localStorage.setItem('theme', theme);
+  }
+  
+  function applyTheme(theme) {
+    document.documentElement.dataset.theme = theme;
+  }
+  
+  
+  function rotateTheme(theme) {
+    if (theme === 'light') {
+      return 'dark'
+    }
+    return 'light';
+  }
+  
+  const themeDisplay = document.getElementById('theme');
+  const themeToggler = document.getElementById('theme-toggle');
+  
+  let theme = getTheme();
+  applyTheme(theme);
+  themeDisplay.innerText = theme;
+  
+  themeToggler.onclick = () => {
+    const newTheme = rotateTheme(theme);
+    applyTheme(newTheme);
+    themeDisplay.innerText = newTheme;
+    saveTheme(newTheme);
+  
+    theme = newTheme;
+  }
 // document.addEventListener('DOMContentLoaded', function () {
 //     const mobileMenu = document.querySelector('.mobile-menu');
 //     const hamburger = document.getElementById('hamburger');
