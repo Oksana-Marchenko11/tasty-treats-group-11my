839b8ae33b1f7de130d0b32da3c943f93e26be36
MarynaOzkara <ozkaramaryna@gmail.com>
2023-08-25 08:45:01 +0300

    Popular: final css

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 import Notiflix from 'notiflix';
 
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
@@ -31,9 +31,7 @@ getRecipes()
 
 // Функція відмалювання популярних рецептів за данних отриманних з беку
 function createMarkupPopularRecepies(arr) {
-    return arr
-        .map(
-            ({ preview, title, description, _id }) => `
+  return arr.map(({ preview, title, description, _id }) => `
       <li class="popular-recepies-item" data-id="${_id}">
           <div class="thomb-popular-wrap">
             <div class="thomb-popular-img">
@@ -46,9 +44,8 @@ function createMarkupPopularRecepies(arr) {
           </div>
       </li>
        
-  `
-        )
-        .join('');
+  `).join('');
+  
 }
 
 // Вішаемо слухач подій на список популярних рецептів
@@ -57,63 +54,62 @@ popularRecepies.addEventListener('click', openModalOnClick);
 // Функція відкриття модалки по кліку на обраний популярний рецепт
 function openModalOnClick(evt) {
     evt.preventDefault();
-
+    
     const resepieID = evt.target.closest('.popular-recepies-item').dataset.id;
-    let modal = require('./modal-recipe.js');
-    modal.default.open(resepieID);
-    // openModal(resepieID);
+    openModal(resepieID);   
 }
 function openModal(id) {
-    fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${id}`)
-        .then(response => response.json())
-        .then(recipe => {
-            // Отримуемо з бекенду рецепт за обраним ID
-            const {
-                title,
-                instructions,
-                preview,
-                rating,
-                time,
-                youtube,
-                ingredients: { desc, id, measure, name },
-                tags,
-            } = recipe;
-            // console.log(recipe);
-            // console.log(description);
-
-            createModalRecepieById(recipe);
-            body.classList.add('no-scroll');
-        })
-        .catch(error => {
-            Notiflix.Notify.failure(
-                'Sorry, there is no information about this recipe. Please try another one.'
-            );
-            console.log(error);
-        });
+  fetch(`https://tasty-treats-backend.p.goit.global/api/recipes/${id}`)
+    .then(response => response.json())
+    .then(recipe => {
+      // Отримуемо з бекенду рецепт за обраним ID
+      const { title, description, preview, rating, time, youtube, ingredients: { desc, id, measure, name }, tags } = recipe;
+      // console.log(recipe);
+      // console.log(description);
+     
+      createModalRecepieById(recipe);
+      const ingredientsList = document.querySelector('.recepi-ingredients-list');
+      const tagsList =document.querySelector('.recepi-tags-list');
+      ingredientsList.innerHTML = createIngredientsMarkup(recipe.ingredients);
+      tagsList.innerHTML = createTagsMarkup(recipe.tags);
+      body.classList.add('no-scroll');
+    })
+    .catch(error => {
+        Notiflix.Notify.failure(
+            'Sorry, there is no information about this recipe. Please try another one.'
+        );
+      console.log(error);
+    });
 }
-
-// Функція відмальовки модального вікна обриного рецепту
-function createModalRecepieById({
-    title,
-    thumb,
-    instructions,
-    preview,
-    rating,
-    time,
-    youtube,
-    ingredients: { desc, id, measure, name },
-    tags,
-}) {
-    const divModalBackdrop = document.createElement('div');
-    divModalBackdrop.classList.add('js-modal');
-    divModalBackdrop.classList.add('is-active');
-    divModalBackdrop.innerHTML = `
+// Функція відмальовки списку ингридіентів
+function createIngredientsMarkup(arr) {
+  return arr.map(({ name, measure}) => `
+  <li class="recepi-ingredients-item">
+    <p class="ingredient-name">${name}</p>
+    <p class="ingredient-mesure">${measure}</p>
+  </li>
+  `).join('');
+}
+// Функція відмальовки списку тегів
+function createTagsMarkup(arr) {
+  return arr.map((tag) => `
+      <li class="recepi-tags-item">
+        <button type="button" class="recepi-tags-btn">#${tag}</button>
+      </li>
+  `).join('');
+}
+// Функція відмальовки модального вікна обраного рецепту
+function createModalRecepieById({ title, preview, instructions, rating, time, youtube, ingredients: { desc, id, measure, name }, tags }) {
+     const divModalBackdrop = document.createElement('div');
+      divModalBackdrop.classList.add('js-modal');
+      divModalBackdrop.classList.add('is-active');
+      divModalBackdrop.innerHTML = `
       <div class="modal-bg">
       <div class="modal-body">
           <div class="modal-close">
-              <button type="button" class="modal-close-btn">x
+              <button type="button" class="modal-close-btn">X
                   <svg class="modal-close-icon" width="16" height="16">
-                      <use href="/src/img/sprite.svg#icon-modal-close"></use>
+                      <use href="/src/images/icons.svg#icon-modal-close"></use>
                   </svg>
               </button>
           </div>
@@ -121,67 +117,42 @@ function createModalRecepieById({
               <div class="video-oder">
                   <h2 class="modal-recepi-title">${title}</h2>
                   <div class="recipe-video-content">
-                      <a href="" class="youtube-link">
+                      <a href="${youtube}" class="youtube-link" target="_blank">
                           <div class="recepi-video-wrap">
                               <img src="${preview}" alt="${title}">
                           </div>
                           <div class="youtube-btn">
                               <svg class="youtube-icon" width="32" height="32">
-                                  <use href="/src/img/youtube.svg#youtube-icon"></use>
+                                  <use href="/src/images/icons.svg#youtube-icon"></use>
                               </svg>
                           </div>
                       </a>
                   </div>
               </div>
               <div class="recipe-info-order">
-                  <div class="recepi-rating">
-                      <p class="recepi-rating-value">${rating}</p>
-                      <div class="recepi-rating-body">
-                          <div class="rating-active">
-                              <div class="rating-items">
-                                  <input type="radio" class="rating-start-item" value="1" name="rating">
-                                  <input type="radio" class="rating-start-item" value="2" name="rating">
-                                  <input type="radio" class="rating-start-item" value="3" name="rating">
-                                  <input type="radio" class="rating-start-item" value="4" name="rating">
-                                  <input type="radio" class="rating-start-item" value="5" name="rating">
-                              </div>
-                          </div>
+                <div class="recepi-rating-wrap">
+                    <div class="recepi-rating">
+                        <p class="recepi-rating-value">${rating}</p>
+                    <div class="recepi-rating-body">
+                        <div class="rating-active">
+                            <div class="rating-items">
+                                <input type="radio" class="rating-stars-item" value="1" name="rating">
+                                <input type="radio" class="rating-stars-item" value="2" name="rating">
+                                <input type="radio" class="rating-stars-item" value="3" name="rating">
+                                <input type="radio" class="rating-stars-item" value="4" name="rating">
+                                <input type="radio" class="rating-stars-item" value="5" name="rating">
+                            </div>
+                        </div>
                       </div>
                       <p class="recepi-time-cook">${time} min</p>
+                </div>
                   </div>
-                  <div class="recepi-ingredients-list list">
-                      <li class="recepi-ingredients-item">
-                          <p class="ingredient-name">Banana</p>
-                          <p class="ingredient-mesure">1 large</p>
-                      </li>
-                      <li class="recepi-ingredients-item">
-                          <p class="ingredient-name">Eggs</p>
-                          <p class="ingredient-mesure">2 medium</p>
-                      </li>
-                      <li class="recepi-ingredients-item">
-                          <p class="ingredient-name">Oil</p>
-                          <p class="ingredient-mesure">1 tsp</p>
-                      </li>
-                      <li class="recepi-ingredients-item">
-                          <p class="ingredient-name">Pecan nuts</p>
-                          <p class="ingredient-mesure">25 tsp</p>
-                      </li>
-                      <li class="recepi-ingredients-item">
-                          <p class="ingredient-name">Baking Powder</p>
-                          <p class="ingredient-mesure">Pinch</p>
-                      </li>
+                  <div class="recepi-ingredients-wrap">
+                        <ul class="recepi-ingredients-list list">
+                        </ul>
                   </div>
                   <div class="recepi-tags-wrap">
                       <ul class="recepi-tags-list list">
-                          <li class="recepi-tags-item">
-                              <button type="button" class="recepi-tags-btn">#Breakfast</button>
-                          </li>
-                          <li class="recepi-tags-item">
-                              <button type="button" class="recepi-tags-btn">#Desert</button>
-                          </li>
-                          <li class="recepi-tags-item">
-                              <button type="button" class="recepi-tags-btn">#Sweet</button>
-                          </li>
                       </ul>
                   </div>
               </div>
@@ -192,33 +163,63 @@ function createModalRecepieById({
               </div>
           </div>
       </div>
-    </div>
+  </div>
       `;
-    document.body.appendChild(divModalBackdrop);
-    //    Створюємо змінну кнопки для модалки і віщаємо обробку події закриття модалки
-    const modalCloseBtn = divModalBackdrop.querySelector('.modal-close-btn');
-    modalCloseBtn.addEventListener('click', closeModal);
+      document.body.appendChild(divModalBackdrop);     
+      //  Cтворення зіркового рейтингу
+// Зірковий рейтинг 
+const ratingValueEl = document.querySelector('.recepi-rating-value');
+const ratingActive = document.querySelector('.rating-active');
+const ratingElements = document.querySelector('.rating-items');
+const ratingValue = Number(ratingValueEl.textContent).toFixed(1);
+ratingValueEl.innerHTML = ratingValue;
+const ratingLength = ratingElements.childElementCount;
+
+function handleRating() {
+if(ratingValue > 0) {
+initRatings();
 }
+}
+
+function initRatings(){
+for(let i = 0; i < ratingLength; i++) {
+setRatingActiveWidth(ratingValue);
+}
+}
+
+// Знаходимо ширину заповнення щирини div з зірочками в процентному відношенні  ( 5 зірочок / 100 = 0.05)
+function setRatingActiveWidth() {
+const ratingActiveWidthAmount = ratingValue / 0.05;
+ratingActive.style.width = `${ratingActiveWidthAmount}%`; 
+// console.log( ratingActive.style.width);
+}
+handleRating();
+
+
+    //    Створюємо змінну кнопки для модалки і віщаємо обробку події закриття модалки
+      const modalCloseBtn = divModalBackdrop.querySelector('.modal-close-btn');
+      modalCloseBtn.addEventListener('click', closeModal);
+}
+
 // Функція закриття модалки
 function closeModal() {
     const modal = document.querySelector('.js-modal.is-active');
     if (modal) {
-        modal.remove();
-        body.classList.remove('no-scroll');
+      modal.remove();
+      body.classList.remove('no-scroll');
     }
-}
+  }
 //   Закриття модалки по кнопкі Escape
-window.addEventListener('keydown', event => {
+  window.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
-        closeModal();
+      closeModal();
     }
-});
+  });
 // Закриття модалки по кліку на область за модалкою
-window.addEventListener('click', event => {
-    if (
-        event.target.classList.contains('modal-close-btn') ||
-        event.target.classList.contains('modal-bg')
-    ) {
-        closeModal();
+  window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal-close-btn') || event.target.classList.contains('modal-bg')) {
+      console.log()
+        closeModal(event.target.elements);
     }
 });
+ 
